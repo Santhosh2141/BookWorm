ae59ec8046bde57dd97876ebeca24b98c3c12dd0
Santhosh2141 <snthshsrinivas@gmail.com>
2025-08-29 20:09:25 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Santhosh Srinivas on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
