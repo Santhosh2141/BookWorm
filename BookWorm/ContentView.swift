b9669cb19e647fe11251a91c1b00a056a3fbbe66
Santhosh2141 <snthshsrinivas@gmail.com>
2025-08-29 20:41:51 +0530

    Bindings and TextEditor

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct PushButton: View{
+    
+    let title: String
+//    @State var isOn: Bool
+    @Binding var isOn: Bool
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View{
+        Button(title){
+            isOn.toggle()
         }
         .padding()
+        .background(LinearGradient(colors: isOn ? onColors: offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+struct ContentView: View {
+    
+    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
+    @AppStorage("notes1") private var notes1 = ""
+    @State private var name = ""
+    @State private var hobby = ""
+    var body: some View {
+        NavigationStack{
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+    //            Toggle("Remember Me", isOn: $rememberMe)
+                // Passing the data of isOn to remember me.
+                PushButton(title: "Remember Me", isOn: $rememberMe)
+                Text(rememberMe ? "On" : "Off")
+                
+                TextField("Enter your text", text: $notes1, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .navigationTitle("Notes")
+                    .padding()
+                
+                Form{
+                    Section{
+                        TextField("Whats your name", text: $name)
+                        TextField("Whats your hobby?", text: $hobby, axis: .vertical)
+                    }
+                }
+                // this creates smth like a notes app. We always need to wrap this w a NavStack or a scrollview or a form
+                TextEditor(text: $notes)
+                    .padding()
+                
+            }
+            .padding()
+            .navigationTitle("Notes")
+        }
+        
     }
 }
 
